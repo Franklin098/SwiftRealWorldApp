7acada9fda3dda253a434cc0ef901aa5b61d32fc
Franklin098 <iefranklin@hotmail.es>
2023-07-30 10:23:05 -0600

    Initial Commit

---
+++ RealWorldApp/RealWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealWorldAppApp.swift
+//  RealWorldApp
+//
+//  Created by Franklin Velásquez on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RealWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
